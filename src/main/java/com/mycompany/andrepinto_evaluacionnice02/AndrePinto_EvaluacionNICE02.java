74625b8a12e80e01f7c1188249af1ab42d617ffa
pintex <andre.pinto9723@gmail.com>
2022-10-26 11:30:10 -0400

    first commit

---
+++ src/main/java/com/mycompany/andrepinto_evaluacionnice02/AndrePinto_EvaluacionNICE02.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.andrepinto_evaluacionnice02;
+
+/**
+ *
+ * @author andre
+ */
+public class AndrePinto_EvaluacionNICE02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
